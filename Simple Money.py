94133e687b990c3d58893504cd45c335b415225a
EriEriel <gwynpond@gmail.com>
2025-10-04 14:12:24 +0700

    Add files via upload

---
+++ Simple Money.py
@@ -0,0 +1,17 @@
+#Simple Program to calcucate amout of banknotes(In this case Thai Baht)between range 2000 - 10000
+
+while True: #create an infinite loop
+    Balance = input("Balance(2000-10,000) : ")
+    if Balance.isdigit(): #check if variable is a digits or not 
+       num = int(Balance)
+       if 2000 <= num <= 10000:
+           a = num//1000
+           b = num//500
+           c = num//100
+           break #break the loop
+       else:
+            print("Balance out of range. Try agian")
+    else:
+        print("please enter a valid number")
+
+input("Press Enter to exit...")
